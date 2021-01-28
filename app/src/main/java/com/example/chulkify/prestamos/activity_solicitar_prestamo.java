57fc047982cbcf5845d84078d3a479b090cacb0d
marmichym46 <marlon.yagualm@ug.edu.ec>
2021-01-27 19:24:05 -0500

    prestamos v1.0
    Signed-off-by: Karen Alejandro <57733313+Karenpar@users.noreply.github.com>

---
+++ app/src/main/java/com/example/chulkify/prestamos/activity_solicitar_prestamo.java
@@ -0,0 +1,16 @@
+package com.example.chulkify.prestamos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chulkify.R;
+
+public class activity_solicitar_prestamo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solicitar_prestamo);
+    }
+}
