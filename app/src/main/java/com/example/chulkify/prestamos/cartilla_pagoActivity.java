fbdba1000303b98d22270189940221a20cccb719
marmichym46 <marlon.yagualm@ug.edu.ec>
2021-02-23 09:54:25 -0500

    soli_pres3.3
    Signed-off-by: marmichym46 <marlon.yagualm@ug.edu.ec>

---
+++ app/src/main/java/com/example/chulkify/prestamos/cartilla_pagoActivity.java
@@ -0,0 +1,16 @@
+package com.example.chulkify.prestamos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chulkify.R;
+
+public class cartilla_pagoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cartilla_pago);
+    }
+}
