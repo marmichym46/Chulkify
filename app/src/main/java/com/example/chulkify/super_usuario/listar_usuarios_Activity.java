7f4da431aa28adcc99d45ea4d9ccc02116dae9c8
marmichym46 <marlon.yagualm@ug.edu.ec>
2021-03-25 12:16:01 -0500

    soli_pres3.9
    Signed-off-by: marmichym46 <marlon.yagualm@ug.edu.ec>

---
+++ app/src/main/java/com/example/chulkify/super_usuario/listar_usuarios_Activity.java
@@ -0,0 +1,16 @@
+package com.example.chulkify.super_usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.chulkify.R;
+
+public class listar_usuarios_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_usuarios);
+    }
+}
